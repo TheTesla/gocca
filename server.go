e964b7544196a718b4096ab1be33067585d33f7c
Stefan Helmert <stefan@smartrns.net>
2019-03-22 16:30:50 +0100

    load files only once

--- server.go
+++ server.go
@@ -3,9 +3,7 @@ package main
 import (
 	"crypto/tls"
 	"crypto/x509"
-//	"crypto/x509/pkix"
 	"crypto/rand"
-//	"encoding/asn1"
 	"encoding/pem"
 	"io/ioutil"
 	"log"
@@ -28,17 +26,33 @@ func main() {
 		//ClientAuth: tls.RequireAndVerifyClientCert,
 		ClientCAs:  caCertPool,
 	}
+	h := handler{}
+	h.SetCAKey("/var/www/api/ca/private/dec.ca.key.pem")
+	h.SetCACert("/var/www/api/ca/certs/ca.cert.pem")
 	srv := &http.Server{
 		Addr:      ":8443",
-		Handler:   &handler{},
+		Handler:   &h,
 		TLSConfig: cfg,
 	}
 	log.Fatal(srv.ListenAndServeTLS(certPath+"/fullchain.pem", certPath+"/privkey.pem"))
 }
 
-type handler struct{}
+type handler struct{
+	caKeyPriv interface{}
+	caCert *x509.Certificate
+}
 
+func (h *handler) SetCAKey(filename string) {
+	caKey, _ := ioutil.ReadFile(filename)
+	caKeyBytes, _ := pem.Decode([]byte(caKey))
+	h.caKeyPriv, _ = x509.ParsePKCS1PrivateKey(caKeyBytes.Bytes)
+}
 
+func (h *handler) SetCACert(filename string) {
+	caCert, _ := ioutil.ReadFile(filename)
+	caCertBytes, _ := pem.Decode([]byte(caCert))
+	h.caCert, _ = x509.ParseCertificate(caCertBytes.Bytes)
+}
 
 func (h *handler) ServeHTTP(w http.ResponseWriter, req *http.Request) {
 	req.ParseForm()
@@ -46,30 +60,9 @@ func (h *handler) ServeHTTP(w http.ResponseWriter, req *http.Request) {
 	fmt.Println(csr)
 	csrBytes, _ := pem.Decode([]byte(csr))
 	csrParsed, _ := x509.ParseCertificateRequest(csrBytes.Bytes)
-	fmt.Println(csrParsed)
-	caKey, err := ioutil.ReadFile("/var/www/api/ca/private/dec.ca.key.pem")
-	fmt.Println(err)
-	caKeyBytes, _ := pem.Decode([]byte(caKey))
-	var caKeyPriv interface{}
-	//caKeyPriv, _ := x509.ParsePKCS8PrivateKey(caKeyBytes.Bytes)
-	caKeyPriv, err = x509.ParsePKCS1PrivateKey(caKeyBytes.Bytes)
-	fmt.Println(err)
-	caKeyPubPEM, err := ioutil.ReadFile("/var/www/api/ca/private/pub.ca.pem")
-	caKeyPubBytes, _ := pem.Decode([]byte(caKeyPubPEM))
-	caKeyPub, err := x509.ParsePKIXPublicKey(caKeyPubBytes.Bytes)
-	fmt.Println(err)
-	fmt.Println("readcert")
-	caCert, err := ioutil.ReadFile("/var/www/api/ca/certs/ca2.cert.pem")
-	fmt.Println(caCert)
-	fmt.Println(err)
-	caCertBytes, _ := pem.Decode([]byte(caCert))
-	caCertParsed, err := x509.ParseCertificate(caCertBytes.Bytes)
-	fmt.Println(err)
-
 	clientCSR := csrParsed
-	caCRT := caCertParsed
+	caCRT := h.caCert
 
-	fmt.Println("A")
 
 	//extSubjectAltName := pkix.Extension{
 	//	Id: asn1.ObjectIdentifier{2, 5, 29, 17},
@@ -91,24 +84,16 @@ func (h *handler) ServeHTTP(w http.ResponseWriter, req *http.Request) {
 		NotAfter:     time.Now().Add(24 * time.Hour),
 		KeyUsage:     x509.KeyUsageDigitalSignature,
 		ExtKeyUsage:  []x509.ExtKeyUsage{x509.ExtKeyUsageClientAuth},
-		//DNSNames:     []string{"san1.srns.local", "san2.srns.local"},
 		DNSNames:     clientCSR.DNSNames,
 		//ExtraExtensions: []pkix.Extension{extSubjectAltName},
 		//Extensions: []pkix.Extension{extSubjectAltName},
 
 	}
 
-	fmt.Println("B")
-	fmt.Println(caKeyPub)
-
-
-	//derBytes, err := x509.CreateCertificate(rand.Reader, &clientCRTTemplate, caCertParsed, caKeyPub, caKeyPriv)
-	derBytes, err := x509.CreateCertificate(rand.Reader, &clientCRTTemplate, caCertParsed, clientCSR.PublicKey, caKeyPriv)
-	fmt.Println("C")
+	derBytes, err := x509.CreateCertificate(rand.Reader, &clientCRTTemplate, h.caCert, clientCSR.PublicKey, h.caKeyPriv)
 	fmt.Println(err)
 	crtPem := pem.EncodeToMemory(&pem.Block{Type: "CERTIFICATE", Bytes: derBytes})
 	fmt.Println(crtPem)
 	w.Write([]byte(crtPem))
-	//w.Write([]byte("PONG\n"))
 }
 
