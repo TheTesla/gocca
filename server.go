0638b3024de8cbbec435e673d62df7fff2a8c06f
Stefan Helmert <stefan@smartrns.net>
2019-03-26 21:10:26 +0100

    always sign new cert

--- server.go
+++ server.go
@@ -1,25 +1,25 @@
 package main
 
 import (
+	"crypto/rand"
+	"crypto/tls"
+	"crypto/x509"
+	"encoding/json"
+	"encoding/pem"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"math/big"
+	"net/http"
 	"os"
 	"os/signal"
 	"syscall"
-	"crypto/tls"
-	"crypto/x509"
-	"crypto/rand"
-	"encoding/pem"
-	"io/ioutil"
-	"log"
-	"net/http"
-	"fmt"
 	"time"
-	"math/big"
-	"encoding/json"
 )
 
 func main() {
 
-        certPath := "/etc/ssl/gotest.smartrns.net"
+	certPath := "/etc/ssl/gotest.smartrns.net"
 	//caCert, err := ioutil.ReadFile("/var/www/api/ca/certs/ca.cert.pem")
 	caCert, err := ioutil.ReadFile("/var/www/api/ca/certs/ca-root.pem")
 	if err != nil {
@@ -28,9 +28,9 @@ func main() {
 	caCertPool := x509.NewCertPool()
 	caCertPool.AppendCertsFromPEM(caCert)
 	cfg := &tls.Config{
-                //ClientAuth: tls.RequestClientCert,
-		ClientAuth: tls.RequireAndVerifyClientCert,
-		ClientCAs:  caCertPool,
+		ClientAuth: tls.RequestClientCert,
+		//ClientAuth: tls.RequireAndVerifyClientCert,
+		ClientCAs: caCertPool,
 		//RootCAs:  caCertPool,
 	}
 	h := handler{}
@@ -38,7 +38,7 @@ func main() {
 	h.SetCACert("/var/www/api/ca/certs/ca-root.pem")
 	c := make(chan os.Signal, 1)
 	signal.Notify(c, syscall.SIGHUP)
-	go func(){
+	go func() {
 		for sig := range c {
 			println(sig)
 			fmt.Printf("Got A HUP Signal! Now Reloading Conf....\n")
@@ -55,9 +55,9 @@ func main() {
 	log.Fatal(srv.ListenAndServeTLS(certPath+"/fullchain.pem", certPath+"/privkey.pem"))
 }
 
-type handler struct{
+type handler struct {
 	caKeyPriv interface{}
-	caCert *x509.Certificate
+	caCert    *x509.Certificate
 }
 
 func (h *handler) SetCAKey(filename string) {
@@ -82,13 +82,19 @@ func (h *handler) ServeHTTP(w http.ResponseWriter, req *http.Request) {
 	dbg2, err := json.Marshal(peerCerts)
 	fmt.Println(err)
 	fmt.Println(string(dbg2))
-	fmt.Println(peerCerts[0].DNSNames)
-	//fmt.Println(csr)
+	//fmt.Println(peerCerts[0].DNSNames)
 	csrBytes, _ := pem.Decode([]byte(csr))
 	csrParsed, _ := x509.ParseCertificateRequest(csrBytes.Bytes)
 	clientCSR := csrParsed
 	caCRT := h.caCert
 
+	//peerDNSNames := peerCerts[0].DNSNames
+	clientDNSNames := clientCSR.DNSNames
+	fmt.Println(clientDNSNames)
+	/*if ok, name := IsSubset(clientDNSNames, peerDNSNames); !ok {
+		fmt.Printf("DNS name not authorized by client certificate: %s\n", name)
+		return
+	}*/
 
 	//extSubjectAltName := pkix.Extension{
 	//	Id: asn1.ObjectIdentifier{2, 5, 29, 17},
@@ -104,14 +110,14 @@ func (h *handler) ServeHTTP(w http.ResponseWriter, req *http.Request) {
 		SignatureAlgorithm: clientCSR.SignatureAlgorithm,
 		PublicKeyAlgorithm: clientCSR.PublicKeyAlgorithm,
 		PublicKey:          clientCSR.PublicKey,
-		SerialNumber: big.NewInt(6),
-		Issuer:       caCRT.Subject,
-		Subject:      clientCSR.Subject,
-		NotBefore:    time.Now(),
-		NotAfter:     time.Now().Add(24 * time.Hour),
-		KeyUsage:     x509.KeyUsageDigitalSignature,
-		ExtKeyUsage:  []x509.ExtKeyUsage{x509.ExtKeyUsageClientAuth},
-		DNSNames:     clientCSR.DNSNames,
+		SerialNumber:       big.NewInt(6),
+		Issuer:             caCRT.Subject,
+		Subject:            clientCSR.Subject,
+		NotBefore:          time.Now(),
+		NotAfter:           time.Now().Add(24 * time.Hour),
+		KeyUsage:           x509.KeyUsageDigitalSignature,
+		ExtKeyUsage:        []x509.ExtKeyUsage{x509.ExtKeyUsageClientAuth},
+		DNSNames:           clientCSR.DNSNames,
 		//ExtraExtensions: []pkix.Extension{extSubjectAltName},
 		//Extensions: []pkix.Extension{extSubjectAltName},
 
@@ -124,3 +130,15 @@ func (h *handler) ServeHTTP(w http.ResponseWriter, req *http.Request) {
 	w.Write([]byte(crtPem))
 }
 
+func IsSubset(x, y []string) (bool, string) {
+	set := make(map[string]bool)
+	for _, v := range y {
+		set[v] = true
+	}
+	for _, v := range x {
+		if _, found := set[v]; !found {
+			return false, v
+		}
+	}
+	return true, ""
+}
